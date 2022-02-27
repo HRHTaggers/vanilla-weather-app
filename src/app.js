6e38c7fde20e14d52a9b6eabd4e091805473d615
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-02-27 20:41:06 +0000

    Addition of weather API connection and variables with updated ids in HTML

--- src/app.js
+++ src/app.js
@@ -34,3 +34,25 @@ document.getElementById("hours").innerHTML =
   (now.getHours() < 10 ? "0" : "") + now.getHours();
 document.getElementById("minutes").innerHTML =
   (now.getMinutes() < 10 ? "0" : "") + now.getMinutes();
+
+//API connector & variables//
+function retrieveWeather(response) {
+  let temperatureElement = document.querySelector(`#temperature`); 
+  let cityElement = document.querySelector(`#city`);
+  let humidityElement = document.querySelector(`#humidity`);
+  let windspeedElement = document.querySelector(`#windspeed`);
+  let weatherDescriptionElement = document.querySelector(`#weather-description`);
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temperature);
+  cityElement.innerHTML = response.data.name;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windspeedElement.innerHTML = Math.round(response.data.wind.speed);
+  weatherDescriptionElement.innerHTML = response.data.weather.description;
+}
+
+let apiKey = `72a4d6e3c49499c57e42e446cad198b6`;
+let units = `metric`;
+let city = `London`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${chosenUnits}&appid=${apiKey}`;
+
+axios.get(apiUrl),then(retrieveWeather);
