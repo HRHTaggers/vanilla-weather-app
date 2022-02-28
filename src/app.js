28e4715da6024329f89d39180ba346c41f2f9dd3
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-02-28 15:53:12 +0000

    Updated JS script for date/time function

--- src/app.js
+++ src/app.js
@@ -33,7 +33,7 @@ function retrieveWeather(response) {
   weatherDescriptionElement.innerHTML = response.data.weather[0].description;
   weatherIconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   weatherIconElement.setAttribute(
     "alt",
