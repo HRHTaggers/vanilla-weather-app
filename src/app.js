a28c37924d9c4d83891932da15f0c3f63635a1bf
HRHTaggers <99132424+HRHTaggers@users.noreply.github.com>
2022-03-02 17:25:22 +0000

    Addition of new weather icons - test

--- src/app.js
+++ src/app.js
@@ -50,9 +50,9 @@ function displayForecast(response) {
       <div class="col-2">
         <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img 
-          src="https://openweathermap.org/img/wn/${
+          src="(src/${
           forecastDay.weather[0].icon
-          }@2x.png";
+          }.png";
           alt=""
           />
           <div class="weather-forecast-temperature">
